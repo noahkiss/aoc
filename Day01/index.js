583a1183d776f271217ba7f8c7fc167e8e159068
Noah Kiss <noah@nkmk.co>
2023-12-09 21:08:43 -0500

    day one solution

--- Day01/index.js
+++ Day01/index.js
@@ -1,33 +1,117 @@
+// ---Day 1: Trebuchet ? !---
+
 const fs = require('fs');
 const path = require('path');
 
 function readInput(fileName) {
-    // Construct the full path for the input file
+    // __dirname gives the directory name of the current module, i.e., DayXX
     const filePath = path.join(__dirname, fileName);
     return fs.readFileSync(filePath, 'utf-8').trim().split('\n');
 }
 
-function sharedLogic(input) {
-    // Common logic or preprocessing steps
-    return input;
-}
+// Something is wrong with global snow production, and you've been selected to
+// take a look. The Elves have even given you a map; on it, they've used stars to
+// mark the top fifty locations that are likely to be having problems.
+
+// You've been doing this long enough to know that to restore snow operations, you
+// need to check all fifty stars by December 25th.
+
+// Collect stars by solving puzzles. Two puzzles will be made available on each
+// day in the Advent calendar; the second puzzle is unlocked when you complete the
+// first. Each puzzle grants one star. Good luck!
+
+// You try to ask why they can't just use a weather machine ("not powerful
+// enough") and where they're even sending you ("the sky") and why your map looks
+// mostly blank ("you sure ask a lot of questions") and hang on did you just say
+// the sky ("of course, where do you think snow comes from") when you realize that
+// the Elves are already loading you into a trebuchet ("please hold still, we need
+// to strap you in").
+
+// As they're making the final adjustments, they discover that their calibration
+// document (your puzzle input) has been amended by a very young Elf who was
+// apparently just excited to show off her art skills. Consequently, the Elves are
+// having trouble reading the values on the document.
+
+// The newly-improved calibration document consists of lines of text; each line
+// originally contained a specific calibration value that the Elves now need to
+// recover. On each line, the calibration value can be found by combining the
+// first digit and the last digit (in that order) to form a single two-digit
+// number.
+
+// For example:
+
+// 1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet
+
+// In this example, the calibration values of these four lines are 12, 38, 15, and
+// 77. Adding these together produces 142.
+
+// Consider your entire calibration document. What is the sum of all of the
+// calibration values?
 
 function partOne(input) {
-    // Your solution for part one goes here
-    return 0;
+    let sum = 0;
+    input.forEach(coord => {
+        const matches = coord.match(/\d/g);
+        const nums = `${matches[0]}${matches[matches.length - 1]}`;
+        sum += parseInt(nums);
+    });
+    return sum;
 }
 
+// Your calculation isn't quite right. It looks like some of the digits are
+// actually spelled out with letters: one, two, three, four, five, six, seven,
+// eight, and nine also count as valid "digits".
+
+// Equipped with this new information, you now need to find the real first and
+// last digit on each line. For example:
+
+// two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen
+
+// In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
+// Adding these together produces 281.
+
+// What is the sum of all of the calibration values?
+
 function partTwo(input) {
-    // Your solution for part two goes here
-    return 0;
+    const numConvert = {
+        "one": "one1one",
+        "two": "two2two",
+        "three": "three3three",
+        "four": "four4four",
+        "five": "five5five",
+        "six": "six6six",
+        "seven": "seven7seven",
+        "eight": "eight8eight",
+        "nine": "nine9nine"
+    };
+
+    let sum = 0;
+    input.forEach(coord => {
+        let c = coord;
+        Object.entries(numConvert).forEach(([key, value]) => {
+            c = c.replace(new RegExp(key, 'g'), value);
+        });
+
+        const matches = c.match(/\d/g);
+        const nums = `${matches[0]}${matches[matches.length - 1]}`;
+        sum += parseInt(nums);
+    });
+    return sum;
 }
 
 function main() {
-    const input = readInput('input.txt'); // Input file in the same directory as the script
-    const processedInput = sharedLogic(input);
-
-    console.log(`Part One: ${partOne(processedInput)}`);
-    console.log(`Part Two: ${partTwo(processedInput)}`);
+    const input = readInput('input.txt');
+    console.log(`Part One: ${partOne(input)}`);
+    console.log(`Part Two: ${partTwo(input)}`);
 }
 
 main();
