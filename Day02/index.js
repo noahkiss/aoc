4dafe77fdddafc424ac7700ba6f3c81cc174f2d5
Noah Kiss <noah@nkmk.co>
2023-12-09 22:15:46 -0500

    add day 2 code

--- Day02/index.js
+++ Day02/index.js
@@ -8,18 +8,59 @@ function readInput(fileName) {
 }
 
 function sharedLogic(input) {
-    // Common logic or preprocessing steps
     return input;
 }
 
 function partOne(input) {
-    // Your solution for part one goes here
-    return 0;
+    let sumIds = 0;
+
+    const constraints = {
+        'r': 12,
+        'g': 13,
+        'b': 14,
+    };
+
+    input.forEach(game => {
+        const gameId = parseInt(game.match(/\d+/)[0]);
+        let isValidGame = true;
+
+        const matches = [...game.matchAll(/(\d+) (red|green|blue)/g)];
+        for (const match of matches) {
+            const number = parseInt(match[1]);
+            const color = match[2][0];
+
+            if (number > constraints[color]) {
+                isValidGame = false;
+                break;
+            }
+        }
+
+        if (isValidGame) sumIds += gameId;
+    });
+
+    return sumIds;
 }
 
 function partTwo(input) {
-    // Your solution for part two goes here
-    return 0;
+    let sumPowers = 0;
+
+    input.forEach(game => {
+        const minReq = { 'r': 0, 'g': 0, 'b': 0 };
+
+        const gameId = parseInt(game.match(/\d+/)[0]);
+
+        const matches = [...game.matchAll(/(\d+) (red|green|blue)/g)];
+        for (const match of matches) {
+            const number = parseInt(match[1]);
+            const color = match[2][0];
+
+            if (number > minReq[color]) minReq[color] = number;
+        }
+
+        sumPowers += Object.values(minReq).reduce((acc, val) => acc * val, 1);
+    });
+
+    return sumPowers;
 }
 
 function main() {
